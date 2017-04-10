af414eb94198114f6575954e5846c03da9113c0b
Sean Pollock <seanpollock3344@gmail.com>
2017-04-09 22:22:23 -0500

    transform method

--- transformer.js
+++ transformer.js
@@ -4,4 +4,21 @@ const babelRC = {
   presets: [require('babel-preset-react-native')]
 }
 
+function transform(src, filename, options) {
+  const babelClonfig = Object.assign({}, babelRC, {
+    filename,
+    sourceFileName: filename,
+  })
+  const result = babel.transform(src, babelConfig)
+  return {
+    ast: result.ast,
+    code: result.code,
+    map: result.map,
+    filename: filename
+  }
+}
+
+
+
+
 // function transform(s)
